3c8e67fc883990806b21e66cd22b767643c0b02c
Mahmud Abdullah <mahmud.rdn@gmail.com>
2023-10-30 03:57:09 +0530

    first commit after adding yolo

---
+++ app/annotate/route.ts
@@ -0,0 +1,54 @@
+import { Ratelimit } from "@upstash/ratelimit";
+import redis from "../../utils/redis";
+import { NextResponse } from "next/server";
+import { headers } from "next/headers";
+import Replicate from "replicate";
+
+// Create a new ratelimiter, that allows 5 requests per 24 hours
+const ratelimit = redis
+  ? new Ratelimit({
+      redis: redis,
+      limiter: Ratelimit.fixedWindow(5, "1440 m"),
+      analytics: true,
+    })
+  : undefined;
+
+export async function POST(request: Request) {
+  // Rate Limiter Code
+  if (ratelimit) {
+    const headersList = headers();
+    const ipIdentifier = headersList.get("x-real-ip");
+
+    const result = await ratelimit.limit(ipIdentifier ?? "");
+
+    if (!result.success) {
+      return new Response(
+        "Too many uploads in 1 day. Please try again in a 24 hours.",
+        {
+          status: 429,
+          headers: {
+            "X-RateLimit-Limit": result.limit,
+            "X-RateLimit-Remaining": result.remaining,
+          } as any,
+        }
+      );
+    }
+  }
+
+  const replicate = new Replicate({
+    auth: process.env.REPLICATE_API_KEY,
+  });
+  const { imageUrl } = await request.json();
+
+  // POST request to Replicate to start the image restoration generation process
+  const output = await replicate.run(
+    "daanelson/yolox:ae0d70cebf6afb2ac4f5e4375eb599c178238b312c8325a9a114827ba869e3e9",
+    {
+      input: {
+        input_image: imageUrl,
+      },
+    }
+  );
+
+  return NextResponse.json(output ? output : "Failed to restore image");
+}
