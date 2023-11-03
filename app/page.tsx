ea36fec2b47ee69e918768bd962be4ebd3bc13a1
himonmist <hasan.mahfuz@linkvisionsoft.com>
2023-11-03 14:29:04 +0600

    background color changed

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,7 @@ export default function HomePage() {
     <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-20 mt-20 background-gradient">
-        <a
-          href="https://vercel.fyi/roomGPT"
-          target="_blank"
-          rel="noreferrer"
-          className="border border-gray-700 rounded-lg py-2 px-4 text-gray-400 text-sm mb-5 transition duration-300 ease-in-out"
-        >
-          Clone and deploy your own with{" "}
-          <span className="text-blue-600">Vercel</span>
-        </a>
+        
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
           Generating dream rooms{" "}
           <span className="relative whitespace-nowrap text-blue-600">
@@ -36,7 +28,7 @@ export default function HomePage() {
         >
           Generate your dream room
         </Link>
-        <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
+        <div className="bg-green flex justify-between items-center w-full flex-col sm:mt-10 mt-6 ">
           <div className="flex flex-col space-y-10 mt-4 mb-16">
             <div className="flex sm:space-x-8 sm:flex-row flex-col">
               <div>
@@ -49,7 +41,7 @@ export default function HomePage() {
                   height={400}
                 />
               </div>
-              <div className="sm:mt-0 mt-8">
+              <div className="sm:mt-0 mt-8 " >
                 <h3 className="mb-1 font-medium text-lg">Generated Room</h3>
                 <Image
                   alt="Generated photo of a room with Decorion"
