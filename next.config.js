7d3ec6d9b7b75256ae91085240ad7549feb7a30a
Mahmud Abdullah <mahmud.rdn@gmail.com>
2023-11-12 18:28:46 +0530

    fix: add googleusercontent.com

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["upcdn.io", "replicate.delivery", "pbxt.replicate.delivery", "california-a.tensordockmarketplace.com", "*.google.com"],
+    domains: ["upcdn.io", "replicate.delivery", "pbxt.replicate.delivery", "california-a.tensordockmarketplace.com", "lh3.googleusercontent.com"],
   },
 };
